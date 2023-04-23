c21e8db64d618c9907bb87b31a4d9fcf2d7fe770
Lisa Reid <lisa@ljreid.com>
2023-04-23 10:33:43 +1000

    2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container")
 const coffees = [
-  { name: "1030", image: "images/coffee1.jpg" },
+  { name: "1032", image: "images/coffee1.jpg" },
   { name: "Voluptatem", image: "images/coffee2.jpg" },
   { name: "Explicabo", image: "images/coffee3.jpg" },
   { name: "Rchitecto", image: "images/coffee4.jpg" },
